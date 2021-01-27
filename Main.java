6748269a531b4dd08a49ae36fe5a30d5c0a1a6ff
mayerj3 <mayerj3@mail.uc.edu>
2021-01-27 04:01:05 +0000

    Added total and doubles of colors

--- Main.java
+++ Main.java
@@ -4,12 +4,12 @@ class Main {
     //total M&Ms
     int mmTotal = 495;
     //
-    double bluePercent = .24;   
-    double brownPercent = .13;
-    double greenPercent = .16;
-    double orangePercent = .20;
-    double redPercent = .13;
-    double yellowPercent = .14;
+    double bluePercent = 0.24;   
+    double brownPercent = 0.13;
+    double greenPercent = 0.16;
+    double orangePercent = 0.20;
+    double redPercent = 0.13;
+    double yellowPercent = 0.14;
 
   }
 }
