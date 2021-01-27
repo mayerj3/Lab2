c2c2eec3c561403776dfefd2f1be9500455673cc
mayerj3 <mayerj3@mail.uc.edu>
2021-01-27 03:57:27 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("M&M Color Counts");
+    //total M&Ms
+    int mmTotal = 495;
+    //
+    double bluePercent = .24;   
+    double brownPercent = .13;
+    double greenPercent = .16;
+    double orangePercent = .20;
+    double redPercent = .13;
+    double yellowPercent = .14;
+
+  }
+}
